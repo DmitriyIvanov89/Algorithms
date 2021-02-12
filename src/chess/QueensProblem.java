41096ef7b99f7db554b5e16874821bc7fad33234
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-02-12 09:59:16 +0300

    queens

--- src/chess/QueensProblem.java
+++ src/chess/QueensProblem.java
@@ -1,7 +1,58 @@
 package chess;
 
 public class QueensProblem {
+
     public static void main(String[] args) {
 
+        QueensProblem Q = new QueensProblem(8);
+        Q.callplaceNQueen();
+
+    }
+
+    int[] board;
+
+    public QueensProblem(int N) {
+        board = new int[N];
+    }
+
+    public boolean canPlaceQueens (int row, int column) {
+        for (int i = 0; i < column; i++) {
+            if (board[i] == row || (board[i] - i) == (row - column) || (board[i] + i) == (row + column)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void placeNQueen (int column, int n) {
+        for (int row = 0; row < n; row++) {
+            if (canPlaceQueens(row,column)) {
+                board[column] = row;
+                if (column == n - 1) {
+                    print(board);
+                } else {
+                    placeNQueen(column + 1, n);
+                }
+            }
+        }
+    }
+
+    public void print (int[] board) {
+        int N = board.length;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i] == j) {
+                    System.out.print("Q ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void callplaceNQueen () {
+        placeNQueen(0, board.length);
     }
 }
