67a294ca749fae43dbcc9d0519f008743bbe048e
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-02-12 17:06:06 +0300

    new queen

--- src/chess/QueensProblem.java
+++ src/chess/QueensProblem.java
@@ -8,13 +8,19 @@ public class QueensProblem {
         Q.callplaceQueen();
 
     }
-
+    /*
+    * Создаем одномерный массив из элементов, каждый из которых будет соответствовать одной из вертикалей.
+    * А значение каждого элемента будет соответствовать той строке, в которой установлен ферзь.
+    */
     int[] board;
 
     public QueensProblem(int N) {
         board = new int[N];
     }
 
+    /*
+     * Метод, проверяющий возможность установки ферзя в текущую клетку,
+     */
     public boolean canPlaceQueens (int row, int column) {
         for (int i = 0; i < column; i++) {
             if (board[i] == row || (board[i] - i) == (row - column) || (board[i] + i) == (row + column)) {
@@ -23,20 +29,24 @@ public class QueensProblem {
         }
         return true;
     }
+    /* Метод поиска с возвратом,
 
-    public void placeQueen (int column, int n) {
-        for (int row = 0; row < n; row++) {
+     */
+    public void placeQueen (int column, int N) {
+        for (int row = 0; row < N; row++) {
             if (canPlaceQueens(row,column)) {
                 board[column] = row;
-                if (column == n - 1) {
+                if (column == N - 1) {
                     print(board);
                 } else {
-                    placeQueen(column + 1, n);
+                    placeQueen(column + 1, N);
                 }
             }
         }
     }
-
+    /* Метод, выводящий в консоль верные решения,
+    честно взято с просторов сети :D
+     */
     public void print (int[] board) {
         int N = board.length;
         for (int i = 0; i < N; i++) {
@@ -51,7 +61,7 @@ public class QueensProblem {
         }
         System.out.println();
     }
-
+    // Вызов метода на весь массив (доску)
     public void callplaceQueen () {
         placeQueen(0, board.length);
     }
